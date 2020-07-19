a9605dc22a5a0bf95af9f9501f8d2a67378ec899
mbandur <67535485+mbandur@users.noreply.github.com>
2020-07-19 18:29:01 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,32 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.utils import resample
-from sklearn.metrics import roc_auc_score, confusion_matrix as cm, classification_report, mean_absolute_error
+from sklearn.metrics import roc_auc_score, confusion_matrix as cm, classification_report
 
-def upsample(X, y):
-    df_all = pd.concat((X, pd.DataFrame({'y': y}, index = y.index)), axis = 1)
-    
-    df_majority = df_all [df_all.y == 0]
-    df_minority = df_all[df_all.y == 1]
-    df_minority_upsampled = resample(df_minority, replace = True, n_samples = df_majority.shape[0], random_state = 100)
-    df_upsampled = pd.concat([df_majority, df_minority_upsampled], axis=0)
-    y_upsampled = df_upsampled.y
-    X_upsampled = df_upsampled.drop('y', axis=1)
-
-    return X_upsampled, y_upsampled
-    
 def vis_results(model, X_train, X_test, y_train, y_test):
         model.fit(X_train,y_train)
         y_pred_train = model.predict(X_train)
-        y_pred_test  = model.predict(X_test)
+        y_pred_test  = model.predict(X_test)  
+        y_probs = model.predict_proba(X_test)[:,1]
         
         report = classification_report(y_test, y_pred_test, output_dict = True)
         confusion_matrix = cm(y_test, y_pred_test)      
-        AUC = roc_auc_score(y_test, y_pred_test) * 2 - 1
-      
-        mae = mean_absolute_error(y_pred_test, y_test)
- 
+        AUC = roc_auc_score(y_test, y_probs) * 2 - 1
+       
         print('Classification Report X_test:')
         print(classification_report(y_test, y_pred_test))
         print('Confusion Matrix:')
@@ -38,10 +24,8 @@ def vis_results(model, X_train, X_test, y_train, y_test):
         sns.despine()
     
         print('AUC: %.2f' % AUC)
-        print('MAE: %.2f' % mae)
-
         print(model)
-        return model, report, AUC, mae
+        return model, report, AUC
 
 def model_result(model_r):
     model_type = type(model_r[0]).__name__
@@ -56,8 +40,7 @@ def model_result(model_r):
     RECALL_1 = model_r[1]['1']['recall']
     F1_1 = model_r[1]['1']['f1-score']
     
-    MAE = model_r[3]
-    return {model_type: [ACC, AUC, PRECISION_ALL, RECALL_ALL, F1_ALL, PRECISION_1, RECALL_1, F1_1, MAE]}   
+    return {model_type: [ACC, AUC, PRECISION_ALL, RECALL_ALL, F1_ALL, PRECISION_1, RECALL_1, F1_1]}   
 
 def max_scores(data):
     max_scores = []
